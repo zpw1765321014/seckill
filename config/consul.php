01b862678d0f69cf03240ba35b962d8e3dd6ee87
admin123456 <1765321014@qq.com>
2021-08-06 20:48:27 +0800

    微服务组件

---
+++ config/consul.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zpw
+ * Date: 2021/8/3
+ * Time: 21:58
+ */
+/**
+ * consul 映射端口
+ */
+return [
+
+];
---
+++ core/consul/Agent.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zpw
+ * Date: 2021/8/3
+ * Time: 21:57
+ */
+
+namespace Core\consul;
+
+
+class Agent
+{
+
+}
